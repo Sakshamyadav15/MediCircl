05e2aaab1a2d12d77efcdf181a259fd39c05a504
Sakshamyadav15 <97941542+Sakshamyadav15@users.noreply.github.com>
2025-03-09 16:40:14 +0530

    test bug fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:medicircle/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MediCircleApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
