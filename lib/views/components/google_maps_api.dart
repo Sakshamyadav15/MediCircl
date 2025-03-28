c41fdbda14c73c6b3843ce0e6cf40e06b4852bfe
Sakshamyadav15 <97941542+Sakshamyadav15@users.noreply.github.com>
2025-03-28 18:49:56 +0530

    chatscreen

--- lib/views/components/google_maps_api.dart
+++ lib/views/components/google_maps_api.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapPopup extends StatefulWidget {
+  const GoogleMapPopup({super.key});
+
   @override
   _GoogleMapPopupState createState() => _GoogleMapPopupState();
 }
